77842ce154ce0e720d05cf43cc5103df159bbcf7
alexanderottosson <caottosson@gmail.com>
2023-01-01 20:47:06 -0800

    Working on TimeLinePage

--- src/Pages/TimeLinePage/TimeLinePage.js
+++ src/Pages/TimeLinePage/TimeLinePage.js
@@ -1,76 +1,81 @@
 //Styles
 import "../../Styles/Style.scss"
 
-import Pics from "./pics"
 
-// import imgas from "../../sources/Waves.png"
+//Sources
+import Pics from "./pics"
 
 function ObjectRender() {
     const data = [
         {   //1
             title: "1. Organizing Supplies",
             bodyText: "The project began with us going through the material supplied by Camosun Innovates. This included an ROV, a pelican case with a monitor, a 100ft tether, and multiple spare parts and accessories that we would come to use during the project.",
-            image: [Pics.wav, Pics.walrus]
+            image: [Pics.ControlStationCase, Pics.ROV]
         },
         {   //2
             title: "2. Initial Proof of Concept & Testing",
             bodyText: "The next step in the project was to test the individual components and systems that we required. This involved multiple weeks of developing the early stages of what would become our final product. We used STM32F407 development boards as a test platform before our PCBs had been finalized and ordered.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.TestBed]
         },
         {   //3
             title: "3. System Integration",
             bodyText: "Once we had proved to ourselves that the individual systems such as the wireless transceivers, sensors, etc worked, it was time to test the entire system as one. This proved more difficult than expected but with perseverance we made it work.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.EndToEndTest]
         },
         {   //4
             title: "4. First Motor Test",
             bodyText: "At this stage of the project we had proved that the overall system worked together. The next step was testing the system with the ROV to see how it would work. This also included measuring the current draw of the motors in order to allow for a proper PCB design.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.MotorControlTest, Pics.CurrentDrawTest]
         },
         {   //5
             title: "5. PCBs Arrived",
             bodyText: "After ordering the PCBs in week 5 of the project we spent most of our time working on more miscellaneous tasks. The PCBs arrived from JLC PCB in week 8 and we quickly began soldering our components so that we could begin testing with them.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.ROVPCBDesign, Pics.ROVPCBFront, Pics.ROVPCBBack, Pics.ControlPCBDesign, Pics.ControlPCBFront, Pics.ControlPCBBack]
         },
         {   //6
             title: "6. Initial Assembly & Testing",
             bodyText: "Once our PCBs had been soldered we quickly began assembling the ROV and all of its components. The image below is from our first fully functioning water test.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.ROVInTheDark]
         },
         {   //7
             title: "7. Second Water Test",
             bodyText: "For our second water test, our goal was to begin fine tuning the control of the ROV as well as to get some footage of it working. We learned a lot of new things that day.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.ROVTurtle, Pics.ROVInPool1]
         },
         {   //8
             title: "8. Third Water Test",
             bodyText: "Our third water test was conducted at Todd Inlet. This day was mainly used for collecting footage that we could use on the symposium day.",
-            image: [Pics.walrus, Pics.wav]
+            image: [Pics.ROVTodd1, Pics.ROVTodd2, Pics.ROVTodd3, Pics.ROVTodd4, Pics.ROVTodd5]
         },
     ]
 
     const tiles = data.map(e => {
+
         return (
-            <div>
-                <h1>{e.title}</h1>
+            <div className="Content">
+                <h3 id="title">{e.title}</h3>
                 <p>{e.bodyText}</p>
-                <img src={e.image[0]} alt="stuff is here"/>
-                {/* <h5>{e.image[0]}</h5> */}
+                <div id="imgContainer">
+                    <img id="y" src={e.image[0]} alt="stuff is here"/>
+                    {/* <img id="y" src={setInterval(ImageShow, 1500)} alt="stuff is here"/> */}
+                </div>
             </div>
         )
     });
 
     return (
-        <div style={{ position: 'relative'}}>
+        <div className="ContentContainer">
             {tiles}
         </div>
     )
-
 }
 
 export default function TimeLinePage() {
 
     return (
-        <ObjectRender />
+        <div className="TimeLineContainer">
+            <ObjectRender />
+        </div>
+        
     );
 }
--- src/Pages/TimeLinePage/pics.js
+++ src/Pages/TimeLinePage/pics.js
@@ -1,5 +1,40 @@
-import wav from "../../sources/Waves.png"
-import walrus from "../../sources/Walrus-Gold.png"
+//1.
+import ControlStationCase from "../../sources/SiteFiles/ControlStationCase.png"
+import ROV from "../../sources/SiteFiles/ROV.png"
 
-export default {wav, walrus};
+//2.
+import TestBed from "../../sources/SiteFiles/EarlyTestBed.png"
+
+//3.
+import EndToEndTest from "../../sources/SiteFiles/EndToEndTest.png"
+
+//4.
+import MotorControlTest from "../../sources/SiteFiles/MotorControlTest.png"
+import CurrentDrawTest from "../../sources/SiteFiles/CurrentDrawTest.png"
+
+//5.
+import ControlPCBBack from "../../sources/SiteFiles/ControlPCBBack.jpg"
+import ControlPCBFront from "../../sources/SiteFiles/ControlPCBFront.jpg"
+import ControlPCBDesign from "../../sources/SiteFiles/ControlPCBDesign.png"
+import ROVPCBFront from "../../sources/SiteFiles/ROVPCBFront.jpg"
+import ROVPCBBack from "../../sources/SiteFiles/ROVPCBBack.jpg"
+import ROVPCBDesign from "../../sources/SiteFiles/ROVPCBDesign.png"
+
+//6.
+import ROVInTheDark from "../../sources/SiteFiles/ROVInTheDark.jpg"
+
+//7.
+import ROVTurtle from "../../sources/SiteFiles/ROVTurtle.png"
+import ROVInPool1 from "../../sources/SiteFiles/ROVInPool1.png"
+
+//8.
+import ROVTodd1 from "../../sources/SiteFiles/ROVTodd1.png"
+import ROVTodd2 from "../../sources/SiteFiles/ROVTodd2.png"
+import ROVTodd3 from "../../sources/SiteFiles/ROVTodd3.png"
+import ROVTodd4 from "../../sources/SiteFiles/ROVTodd4.png"
+import ROVTodd5 from "../../sources/SiteFiles/ROVTodd5.png"
+
+const y = { ControlStationCase, ROV, TestBed, EndToEndTest, MotorControlTest, CurrentDrawTest, ControlPCBBack, ControlPCBFront, ControlPCBDesign, ROVPCBFront, ROVPCBBack, ROVPCBDesign, ROVInTheDark, ROVTurtle, ROVInPool1, ROVTodd1, ROVTodd2, ROVTodd3, ROVTodd4, ROVTodd5 };
+
+export default y;
 // export default walrus;
